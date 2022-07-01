3cc5f4151823b068c9055ace8be1ee07e78d5588
younjaeyeal <jyyoun1021@gmail.com>
2022-07-02 00:12:08 +0900

    log 출력

---
+++ src/main/java/codej/codemarket/constant/Method.java
@@ -0,0 +1,5 @@
+package codej.codemarket.constant;
+
+public enum Method {
+    GET,POST,PUT,PATCH,DELETE
+}
--- src/main/java/codej/codemarket/controller/BoardController.java
+++ src/main/java/codej/codemarket/controller/BoardController.java
@@ -1,7 +1,9 @@
 package codej.codemarket.controller;
 
+import codej.codemarket.constant.Method;
 import codej.codemarket.domain.BoardDTO;
 import codej.codemarket.service.BoardService;
+import codej.codemarket.util.UiUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
@@ -16,7 +18,7 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/board")
-public class BoardController {
+public class BoardController extends UiUtils {
 
     private final BoardService boardService;
 
@@ -40,19 +42,20 @@ public class BoardController {
 
     }
     @PostMapping("/register")
-    public String register(final BoardDTO boardDTO){
+    public String register(final BoardDTO boardDTO,Model model){
         try{
             boolean isRegistered = boardService.registerBoard(boardDTO);
             if(isRegistered == false){
-                //게시글 등록이 실패하였다는 메시지를 전달
+                return showMessageWithRedirect("게시글 등록에 실패했습니다.","/board/list", Method.GET,null,model);
             }
         }catch (DataAccessException e){
             //데이터 베이스 처리 과정에 문제가 발생하였다는 메시지를 전달
-            e.printStackTrace();
+            return showMessageWithRedirect("데이터베이스 처리 과정에 문제가 생겼습니다.","/board/list",Method.GET,null,model);
         }catch (Exception e){
             //시스템에 문제가 발생하였다는 메시지를 전달
+            return showMessageWithRedirect("시스템에 문제가 발생하였습니다.","/board/list",Method.GET,null,model);
         }
-        return "redirect:/board/list";
+        return showMessageWithRedirect("게시글 등록이 완료되었습니다.","/board/list",Method.GET,null,model);
     }
     @GetMapping("/list")
     public void boardList(Model model){
@@ -76,21 +79,25 @@ public class BoardController {
 
     }
     @PostMapping("/delete")
-    public String delete(@RequestParam(value = "idx",required = false)Long idx){
+    public String delete(@RequestParam(value = "idx",required = false)Long idx,Model model){
         if(idx == null){
             //올바르지 않은 접근이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
-            return "redirect:/board/list";
+            return showMessageWithRedirect("올바르지 않은 접근입니다.","/board/lit",Method.GET,null,model);
         }
         try{
             boolean isDeleted = boardService.deleteBoard(idx);
             if(isDeleted == false){
                 //게시글 삭제에 실패했다는 메시지 전달
+                return showMessageWithRedirect("게시글 삭제에 실패했습니다.","/board/list",Method.GET,null,model);
             }
         }catch (DataAccessException e){
             //데이터베이스 처리 과정에 문제가 발생했다는 메시지 전달
+            return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/board/list", Method.GET, null, model);
         }catch (Exception e){
             //시스템에 문제가 발생했다는 메시지 전달
+            return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/board/list", Method.GET, null, model);
         }
-        return "redirect:/board/list";
+        return showMessageWithRedirect("게시글 삭제가 완료되었습니다.", "/board/list", Method.GET, null, model);
     }
-}
+    }
+
