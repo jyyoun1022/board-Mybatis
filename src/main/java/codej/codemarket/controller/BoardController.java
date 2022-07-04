5440d044f2b4cc236a5538ad9c73dd5e1e8806a7
younjaeyeal <jyyoun1021@gmail.com>
2022-07-04 19:38:02 +0900

    makeQueryString 으로 게시글 돌아올때 그대로

--- src/main/java/codej/codemarket/controller/BoardController.java
+++ src/main/java/codej/codemarket/controller/BoardController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,15 +24,15 @@ public class BoardController extends UiUtils {
 
 
     @GetMapping("/write")
-    public String write(Model model,
+    public String write(@ModelAttribute("params")BoardDTO parmas, Model model,
                       @RequestParam(value = "idx",required = false) Long idx){
 
         if(idx == null){
             model.addAttribute("board",new BoardDTO());
         }else{
             BoardDTO boardDTO = boardService.getBoardDetail(idx);
-            if( boardDTO == null){
-                return "redirect:/board/list";
+            if( boardDTO == null || "Y".equals(boardDTO.getDeleteYn())){
+                return showMessageWithRedirect("없는 게시글이거나 이미 삭제된 게시글입니다.", "/board/list", Method.GET, null, model);
             }
             model.addAttribute("board",boardDTO);
 
@@ -40,9 +41,9 @@ public class BoardController extends UiUtils {
 
     }
     @PostMapping("/register")
-    public String register(final BoardDTO boardDTO,Model model){
+    public String register(@ModelAttribute("params")final BoardDTO params,Model model){
         try{
-            boolean isRegistered = boardService.registerBoard(boardDTO);
+            boolean isRegistered = boardService.registerBoard(params);
             if(isRegistered == false){
                 return showMessageWithRedirect("게시글 등록에 실패했습니다.","/board/list", Method.GET,null,model);
             }
@@ -67,41 +68,44 @@ public class BoardController extends UiUtils {
 
     }
     @GetMapping("/read")
-    public String boardDetail(Model model,
+    public String boardDetail(@ModelAttribute("params")BoardDTO params,
+                              Model model,
                               @RequestParam(value = "idx",required = false)Long idx){
         if(idx == null){
             //올바르지 않은 접근이라는 메시지를 전달,게시글리스트로 리다이렉트
-            return "redirect:/board/list";
+            return showMessageWithRedirect("올바르지 않은 접근 입니다.","/board/list",Method.GET,null,model);
         }
         BoardDTO boardDTO = boardService.getBoardDetail(idx);
         if(boardDTO == null || "Y".equals(boardDTO.getDeleteYn())){
             //없는 게시글이거나, 이미 삭제된 게시글이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
-            return "redirect:/board/list";
+            return showMessageWithRedirect("없는 게시글이거나 이미 삭제된 게시글입니다.", "/board/list", Method.GET, null, model);
         }
         model.addAttribute("board",boardDTO);
         return "/board/read";
 
     }
     @PostMapping("/delete")
-    public String delete(@RequestParam(value = "idx",required = false)Long idx,Model model){
+    public String delete(@ModelAttribute("params")BoardDTO params,@RequestParam(value = "idx",required = false)Long idx,Model model){
         if(idx == null){
             //올바르지 않은 접근이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
             return showMessageWithRedirect("올바르지 않은 접근입니다.","/board/lit",Method.GET,null,model);
         }
+
+        Map<String, Object> pagingParams = getPagingParams(params);
         try{
             boolean isDeleted = boardService.deleteBoard(idx);
             if(isDeleted == false){
                 //게시글 삭제에 실패했다는 메시지 전달
-                return showMessageWithRedirect("게시글 삭제에 실패했습니다.","/board/list",Method.GET,null,model);
+                return showMessageWithRedirect("게시글 삭제에 실패했습니다.","/board/list",Method.GET,pagingParams,model);
             }
         }catch (DataAccessException e){
             //데이터베이스 처리 과정에 문제가 발생했다는 메시지 전달
-            return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/board/list", Method.GET, null, model);
+            return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/board/list", Method.GET, pagingParams, model);
         }catch (Exception e){
             //시스템에 문제가 발생했다는 메시지 전달
-            return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/board/list", Method.GET, null, model);
+            return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/board/list", Method.GET, pagingParams, model);
         }
-        return showMessageWithRedirect("게시글 삭제가 완료되었습니다.", "/board/list", Method.GET, null, model);
+        return showMessageWithRedirect("게시글 삭제가 완료되었습니다.", "/board/list", Method.GET, pagingParams, model);
     }
     }
 
--- src/main/java/codej/codemarket/util/UiUtils.java
+++ src/main/java/codej/codemarket/util/UiUtils.java
@@ -1,25 +1,39 @@
 package codej.codemarket.util;
 
 import codej.codemarket.constant.Method;
+import codej.codemarket.paging.Criteria;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Controller
 public class UiUtils {
 
-    public String showMessageWithRedirect(@RequestParam(value = "message",required = false) String message,
-                                          @RequestParam(value = "redirectUri",required = false)String redirectUri,
-                                          @RequestParam(value = "method",required = false)Method method,
-                                          @RequestParam(value = "params",required = false) Map<String,String> params, Model model){
-        model.addAttribute("message",message);
-        model.addAttribute("redirectUri",redirectUri);
-        model.addAttribute("method",message);
-        model.addAttribute("params",params);
+    public String showMessageWithRedirect(@RequestParam(value = "message", required = false) String message,
+                                          @RequestParam(value = "redirectUri", required = false) String redirectUri,
+                                          @RequestParam(value = "method", required = false) Method method,
+                                          @RequestParam(value = "params", required = false) Map<String, Object> params, Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("redirectUri", redirectUri);
+        model.addAttribute("method", message);
+        model.addAttribute("params", params);
 
         return "utils/message-redirect";
 
+    }
+
+    public Map<String, Object> getPagingParams(Criteria criteria) {
+
+        Map<String, Object> params = new LinkedHashMap<>();
+        params.put("currentPageNo", criteria.getCurrentPageNo());
+        params.put("recordsPerPage", criteria.getRecordsPerPage());
+        params.put("pageSize", criteria.getPageSize());
+        params.put("searchType", criteria.getSearchType());
+        params.put("searchKeyword", criteria.getSearchKeyword());
+
+        return params;
     }
 }
