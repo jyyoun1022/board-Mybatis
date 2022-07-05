0b9625573b2c57cbf35d151c8dca8adf068d7de5
younjaeyeal <jyyoun1021@gmail.com>
2022-07-05 20:28:15 +0900

    댓글 등록 처리

--- src/main/java/codej/codemarket/controller/CommentController.java
+++ src/main/java/codej/codemarket/controller/CommentController.java
@@ -8,6 +8,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,6 +39,26 @@ public class CommentController {
             jsonObj.add("commentList",jsonArr);
         }
 
+        return jsonObj;
+    }
+
+    @RequestMapping(value = {"","/{idx}"},method = {RequestMethod.POST,RequestMethod.PATCH})
+    public JsonObject registerComment(@PathVariable(value = "idx",required = false)Long idx,
+                                      @RequestBody final CommentDTO params){
+
+        JsonObject jsonObj = new JsonObject();
+
+        try{
+            if( idx != null){
+                params.setIdx(idx);
+            }
+            boolean isRegistered = commentService.registerComment(params);
+            jsonObj.addProperty("result",isRegistered);
+        }catch (DataAccessException e){
+            jsonObj.addProperty("message","데이터 처리과정에서 문제가 발생하였습니다.");
+        }catch (Exception e){
+            jsonObj.addProperty("message","시스템 문제가 발생하였습니다.");
+        }
         return jsonObj;
     }
 
--- src/main/java/codej/codemarket/lab/lab.java
+++ src/main/java/codej/codemarket/lab/lab.java
@@ -4,22 +4,7 @@ public class lab {
 
     public static void main(String[] args) {
 
-        int a = 99;
-        int b = 10;
-
-        double c = 98;
-        double d = 10;
-
-
-        int an = 8;
-        double bn = 12;
-
-        System.out.println(an * bn);
-
-
-        System.out.println((int)((a-1)/b)+1);
-        System.out.println((a/b)+(a % b >0 ? 1 : 0));
-        System.out.println((int)Math.ceil(a/b));
-        System.out.println((int)Math.ceil(a-1)/b+1);
+        int a = 0;
     }
+
 }
